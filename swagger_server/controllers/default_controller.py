f76359448d04f7363526389de5c274b0a383c957
akifyuksel <akif42@live.nl>
2022-02-18 19:04:03 +0100

    removed redundant body def

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -30,7 +30,6 @@ def delete_student(student_id):  # noqa: E501
     :rtype: Student
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
         return delete(student_id)
     return 500, 'error'
 
@@ -46,6 +45,5 @@ def get_student_by_id(student_id):  # noqa: E501
     :rtype: Student
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
         return get_student_by_id(student_id)
     return 500, 'error'
